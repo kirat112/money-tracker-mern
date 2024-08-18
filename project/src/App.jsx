2a865fc225982620b2f820ae03063019ac3d592a
kirat112 <harkiratsingh1128@gmail.com>
2024-08-18 23:34:56 +0530

    init

---
+++ project/src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div className="basic">
+          <input type="text" placeholder={'+200 new samsung tv'}/>
+          <input type="datetime-local"/>
+        </div>
+        <div className="description">
+          <input type="text" placeholder={'description'}/>
+        </div>
+        <button type="submit">Add new transaction</button>
+      </form>
+      <div className="transaction">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung TV</div>
+            <div className="description">It was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price red">-$500</div>
+            <div className="datetime">2023-12-18 15:45</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Gig job new website</div>
+            <div className="description">Salary came</div>
+          </div>
+          <div className="right">
+            <div className="price green">+$400</div>
+            <div className="datetime">2023-12-18 15:45</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Iphone</div>
+            <div className="description">It was time for new phone</div>
+          </div>
+          <div className="right">
+            <div className="price red">-$500</div>
+            <div className="datetime">2023-12-18 15:45</div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
