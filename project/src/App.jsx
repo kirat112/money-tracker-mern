5fd597e1d56288c5867a2f30293c6009e5508a8b
Ankush Makkar <ankushmakkar108@gmail.com>
2024-08-19 01:34:53 +0530

    pushhshh

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,53 +1,131 @@
-import './App.css'
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { cloneElement, useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  let x = 1; // change render _> reset
+  const [xs, setXs] = useState(1); // change render
+  const xr = useRef(1); // no renreder ' but bhulda ni
+
+  const [txns, setTxns] = useState([]);
+
+  const [formData, setFormData] = useState({
+    price: "",
+    time: "",
+    desc: "",
+  });
+
+  useEffect(() => {
+    // backenf upadte logic
+    console.log("lcoal storage upadte logic  called");
+    if (txns.length > 0) localStorage.setItem("txns", JSON.stringify(txns));
+  }, [txns]);
+
+  useEffect(() => {
+    // api call
+    const oldTxn = localStorage.getItem("txns");
+    if (oldTxn) {
+      // pai hai
+      setTxns(JSON.parse(oldTxn));
+    }
+  }, []);
+
+  console.log("rerenderd");
+
+  const handleChange = (e) => {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const logx = () => {
+    console.log(x);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const orignalTxns = [...txns];
+    orignalTxns.push(formData);
+    setTxns(orignalTxns);
+    setFormData({
+      price: "",
+      time: "",
+      desc: "",
+    });
+  };
+
   return (
     <main>
-      <h1>$400<span>.00</span></h1>
-      <form>
+      <h1>
+        $400<span>.00</span>
+      </h1>
+      <form onSubmit={handleSubmit}>
         <div className="basic">
-          <input type="text" placeholder={'+200 new samsung tv'}/>
-          <input type="datetime-local"/>
+          <input
+            value={formData.price}
+            name="price"
+            type="number"
+            onChange={handleChange}
+            placeholder={"+200"}
+          />
+          <input
+            value={formData.time}
+            onChange={handleChange}
+            name="time"
+            type="datetime-local"
+          />
         </div>
         <div className="description">
-          <input type="text" placeholder={'description'}/>
+          <input
+            value={formData.desc}
+            onChange={handleChange}
+            name="desc"
+            type="text"
+            placeholder={"description"}
+          />
         </div>
         <button type="submit">Add new transaction</button>
       </form>
       <div className="transactions">
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New Samsung TV</div>
-            <div className="description">It was time for new tv</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$500</div>
-            <div className="datetime">2023-12-18 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Gig job new website</div>
-            <div className="description">Salary came</div>
-          </div>
-          <div className="right">
-            <div className="price green">+$400</div>
-            <div className="datetime">2023-12-18 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Iphone</div>
-            <div className="description">It was time for new phone</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$500</div>
-            <div className="datetime">2023-12-18 15:45</div>
-          </div>
-        </div>
+        {txns.length > 0 &&
+          txns.map((txn, index) => {
+            return <Transaction key={index} txn={txn} />;
+          })}
       </div>
+      {/* <button onClick={logx}>logx</button>
+      <button onClick={() => x++}>innc x : {x}</button>
+      <button onClick={() => setXs((prev) => prev + 1)}>innc xs : {xs}</button>
+      <button onClick={() => (xr.current = xr.current + 1)}>
+        innc xr : {xr.current}
+      </button> */}
     </main>
-  )
+  );
 }
 
-export default App
+const Transaction = ({ txn }) => {
+  return (
+    <div className="transaction">
+      <div className="left">
+        <div className="name">{txn.desc}</div>
+        <div className="description">{txn.desc}</div>
+      </div>
+      <div className="right">
+        {/* {txn.price < 0 ? (
+          <div className="price red">{txn.price}</div>
+        ) : (
+          <div className="price green">{txn.price}</div>
+          )} */}
+        <div className={"price " + (txn.price > 0 ? "green" : "red")}>
+          {txn.price}
+        </div>
+        <div className="datetime">{txn.time}</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
